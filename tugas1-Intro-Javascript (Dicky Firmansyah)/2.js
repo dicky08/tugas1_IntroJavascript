ab43f4d38093ae5dec69c62fddb16dab5ca33eb3
dicky08 <dickyf147@gmail.com>
2020-08-08 06:35:30 +0700

    Perubahan No 2

--- tugas1-Intro-Javascript (Dicky Firmansyah)/2.js
+++ tugas1-Intro-Javascript (Dicky Firmansyah)/2.js
@@ -1,36 +1,35 @@
 // // Membuat Rata-Rata Nilai UN
 
 function nilai(mtk, bhsIndo, bhsIngg, ipa) {
-
-    let nilaiUn = [mtk, bhsIndo, bhsIngg, ipa];
-    let panjangArray = nilaiUn.length;
-    let nilaiAwal = 0;
-    for (let i = 0; i < nilaiUn.length; i++) {
-        if (typeof nilaiUn[i] === 'undefined') {
-            return 'Inputan tidak boleh kosong';
-        } else if (typeof nilaiUn[i] !== 'number') {
-            return 'Yang diinputkan bukan angka';
-        } else {
-            nilaiAwal += nilaiUn[i] / panjangArray;
-        }
-    }
-    let result = nilaiAwal;
-    if ((result >= 90) && (result <= 100)) {
-        return (`Rata-rata = ${result}
-Grade = A`);
-    } else if ((result >= 80) && result <= 89) {
-        return (`Rata-rata = ${result}
-Grade = B`);
-    } else if ((result >= 70) && result <= 79) {
-        return (`Rata-rata = ${result}
-Grade = E`);
-    } else if ((result >= 60) && result <= 69) {
-        return (`Rata-rata = ${result}
-Grade = E`);
+  let nilaiUn = [mtk, bhsIndo, bhsIngg, ipa];
+  let panjangArray = nilaiUn.length;
+  let nilaiAwal = 0;
+  for (let i = 0; i < nilaiUn.length; i++) {
+    if (typeof nilaiUn[i] === "undefined") {
+      return "Inputan tidak boleh kosong";
+    } else if (typeof nilaiUn[i] !== "number") {
+      return "Yang di inputkan bukan angka";
     } else {
-        return (`Rata-rata = ${result}
-Grade = E`);
+      nilaiAwal += nilaiUn[i] / panjangArray;
     }
+  }
+  let result = nilaiAwal;
+  if (result >= 90 && result <= 100) {
+    return `Rata-rata = ${result}
+Grade = A`;
+  } else if (result >= 80 && result <= 89) {
+    return `Rata-rata = ${result}
+Grade = B`;
+  } else if (result >= 70 && result <= 79) {
+    return `Rata-rata = ${result}
+Grade = E`;
+  } else if (result >= 60 && result <= 69) {
+    return `Rata-rata = ${result}
+Grade = E`;
+  } else {
+    return `Rata-rata = ${result}
+Grade = E`;
+  }
 }
 let diki = nilai(80, 90, 70, 100);
 let budi = nilai(100, 90, 20, 80);
